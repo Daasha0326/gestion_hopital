c74cabffc11df2914442223e0e09689ab4820576
Khadydija <khadygueye1002@gmail.com>
2023-12-18 12:00:44 +0000

    first commit

---
+++ back-end_laravel/app/Models/Patient.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Patient extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function rendezVouse()
+    {
+        return $this->hasMany(RendezVous::class);
+    }
+}
---
+++ back-end_laravel/routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\AdminContrller;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PropertController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('listePatient/user', [UserController::class, 'listePatient']);
+    Route::get('nbrePatient', [UserController::class, 'nbrePatientParJour']);
+    Route::post('createDossier', [UserController::class, 'createDossierMedical']);
+});
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout/{id}', [AuthController::class, 'logout']);
+
+//Routes pour l'administrateur
+Route::post('/ajout/user', [AdminController::class, 'store']);
+Route::get('/user/{id}', [AdminController::class, 'show']);
+Route::post('update/user', [AdminController::class, 'update']);
+Route::get('/delete/user', [AdminController::class, 'destroy']);
